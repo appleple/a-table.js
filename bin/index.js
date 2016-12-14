39acedc411f1486c35258170bd3c41917fe71582
steelydylan <ess_president@me.com>
2016-12-15 02:18:34 +0900

    move to lib

--- bin/index.js
+++ bin/index.js
@@ -6,4 +6,4 @@ var text = fs.readFileSync(path.resolve(process_path, "./src/index.js"), 'utf8')
 text = text.replace(/import (.*?) from '((.*?)\.(html|css))'/g,function(a,b,c){
 	return "var "+ b +" = \`"+fs.readFileSync(path.resolve(process_path,"./src/",c), 'utf8')+"\`";
 });
-fs.writeFileSync(path.resolve(process_path,"./prebuild.js"),text, 'utf8');
+fs.writeFileSync(path.resolve(process_path,"./lib/prebuild.js"),text, 'utf8');
