7c6cb20eb9ed4d1b7b26cac2c14c7d8f0b5c4227
steelydylan <ess_president@me.com>
2016-12-14 11:52:06 +0900

    add focus last feature

--- index.js
+++ index.js
@@ -405,6 +405,23 @@ var Spread = function (_aTemplate) {
       var point = this.getLargePoint.apply(null, points);
       var width = point.width;
       var $th = $('.js-table-header th', "[data-id='" + this.id + "']");
+      var elem = $(".spread-table-selected .spread-table-editable", "[data-id='" + this.id + "']")[0];
+      if (elem) {
+        elem.focus();
+        if (typeof window.getSelection != "undefined" && typeof document.createRange != "undefined") {
+          var range = document.createRange();
+          range.selectNodeContents(elem);
+          range.collapse(false);
+          var sel = window.getSelection();
+          sel.removeAllRanges();
+          sel.addRange(range);
+        } else if (typeof document.body.createTextRange != "undefined") {
+          var textRange = document.body.createTextRange();
+          textRange.moveToElementText(elem);
+          textRange.collapse(false);
+          textRange.select();
+        }
+      }
       $th.each(function (i) {
         if (i > width) {
           $(this).remove();
