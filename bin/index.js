f467f335d63f64d8120666e438e9ba5db466a265
steelydylan <ess_president@me.com>
2017-01-31 23:16:19 +0900

    Update gh-page

--- bin/index.js
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env node
-var fs = require("fs");
-var process_path = process.cwd();
-var path = require('path');
-var text = fs.readFileSync(path.resolve(process_path, "./src/index.js"), 'utf8');
-text = text.replace(/import (.*?) from '((.*?)\.(html|css))'/g,function(a,b,c){
-	return "var "+ b +" = \`"+fs.readFileSync(path.resolve(process_path,"./src/",c), 'utf8')+"\`";
-});
-fs.writeFileSync(path.resolve(process_path,"./lib/prebuild.js"),text, 'utf8');
