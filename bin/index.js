ee8b1ab3cdfdecb753fc52a466d756e294fbaf94
steelydylan <ess_president@me.com>
2017-03-17 21:34:58 +0900

    fix

--- bin/index.js
+++ bin/index.js
@@ -1,10 +1,11 @@
 #! /usr/bin/env node
 var fs = require("fs");
+var jsesc = require("jsesc");
 var process_path = process.cwd();
 var path = require('path');
 var text = fs.readFileSync(path.resolve(process_path, "./src/index.js"), 'utf8');
 text = text.replace(/import (.*?) from '((.*?)\.(html|css))'/g,function(a,b,c){
-  var template = fs.readFileSync(path.resolve(process_path,"./src/",c), 'utf8').replace(/\n/g,"").replace(/\\/g,"\\\\");
+  var template = jsesc(fs.readFileSync(path.resolve(process_path,"./src/",c), 'utf8'));
 	return "var "+ b +" = \'"+template+"\'";
 });
 fs.writeFileSync(path.resolve(process_path,"./lib/prebuild.js"),text, 'utf8');
