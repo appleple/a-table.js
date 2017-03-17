bf4d463eed0fc73250888fb1c826af94a9ef7b08
steelydylan <ess_president@me.com>
2017-03-17 20:28:33 +0900

    fix bug when import

--- bin/index.js
+++ bin/index.js
@@ -4,7 +4,7 @@ var process_path = process.cwd();
 var path = require('path');
 var text = fs.readFileSync(path.resolve(process_path, "./src/index.js"), 'utf8');
 text = text.replace(/import (.*?) from '((.*?)\.(html|css))'/g,function(a,b,c){
-  var template = fs.readFileSync(path.resolve(process_path,"./src/",c), 'utf8').replace(/\n/g,"");
+  var template = fs.readFileSync(path.resolve(process_path,"./src/",c), 'utf8').replace(/\n/g,"").replace(/\\/g,"\\\\");
 	return "var "+ b +" = \'"+template+"\'";
 });
 fs.writeFileSync(path.resolve(process_path,"./lib/prebuild.js"),text, 'utf8');
