0034b9c30f7d1294273c627dbc36613fcb4716a4
steelydylan <ess_president@me.com>
2017-05-15 21:21:29 +0900

    v1.2.3

--- build/a-table.min.js
+++ build/a-table.min.js
@@ -5,7 +5,7 @@
  * a-table:
  *   license: MIT (http://opensource.org/licenses/MIT)
  *   author: appleple
- *   version: 1.2.2
+ *   version: 1.2.3
  *
  * a-template:
  *   license: MIT (http://opensource.org/licenses/MIT)
