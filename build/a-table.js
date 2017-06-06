6e5ea02b16b87d6ccf2a217b406387d1441e192e
steelydylan <ess_president@me.com>
2017-06-07 01:19:16 +0900

    v1.4.5

--- build/a-table.js
+++ build/a-table.js
@@ -5,7 +5,7 @@
  * a-table:
  *   license: MIT (http://opensource.org/licenses/MIT)
  *   author: appleple
- *   version: 1.4.4
+ *   version: 1.4.5
  *
  * a-template:
  *   license: MIT (http://opensource.org/licenses/MIT)
