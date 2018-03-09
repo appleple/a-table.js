e227b7265981080a280f27c30201f41855b1844e
Hori Godai <ess_president@me.com>
2018-03-09 15:28:59 +0900

    v1.5.2

--- build/a-table.min.js
+++ build/a-table.min.js
@@ -5,7 +5,7 @@
  * a-table:
  *   license: MIT (http://opensource.org/licenses/MIT)
  *   author: appleple
- *   version: 1.5.1
+ *   version: 1.5.2
  *
  * a-template:
  *   license: MIT (http://opensource.org/licenses/MIT)
