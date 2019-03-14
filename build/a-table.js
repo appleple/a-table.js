c6f84e5785fe799fdfbc6108a2acbd986ed4ea01
Hori Godai <ess_president@me.com>
2019-03-14 17:45:02 +0900

    v1.4.18

--- build/a-table.js
+++ build/a-table.js
@@ -5,7 +5,7 @@
  * a-table:
  *   license: MIT (http://opensource.org/licenses/MIT)
  *   author: appleple
- *   version: 1.4.17
+ *   version: 1.4.18
  *
  * a-template:
  *   license: MIT (http://opensource.org/licenses/MIT)
