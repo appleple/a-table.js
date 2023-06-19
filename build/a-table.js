5c3fc9925d879331b161d63b82d826ae6db7142d
Takayuki Nagatomi <takayuki@TakayukinoMacBook-Pro.local>
2023-06-19 10:49:01 +0900

    v1.5.10

--- build/a-table.js
+++ build/a-table.js
@@ -5,7 +5,7 @@
  * a-table:
  *   license: MIT (http://opensource.org/licenses/MIT)
  *   author: appleple
- *   version: 1.5.9
+ *   version: 1.5.10
  *
  * a-template:
  *   license: MIT (http://opensource.org/licenses/MIT)
